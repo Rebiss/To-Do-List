d248fa7ae280d1bb93cffe293d31d3f30e391cba
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-05-09 00:25:59 +0400

    rm: Test, Service

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDo from './componet/ToDo/ToDo';
-import * as serviceWorker from './service/serviceWorker';
 
 ReactDOM.render(<ToDo />, document.getElementById('root'));
 serviceWorker.unregister();
